26dedc933e0a213f2930a616721f3290b4b07e1b
hoareausylvie <hoareauylvie@outlook.fr>
2025-09-02 21:28:36 +0400

    feat: ajout de la collecte de cerise

---
+++ Assets/Scripts/CherryCollect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CherryCollect : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // other.GetComponent<PlayerHealth>()?.AddHealth(1);
+            Destroy(gameObject); // détruit la cerise
+
+            Debug.Log("Cerise collectée !");
+        }
+    }
+}
